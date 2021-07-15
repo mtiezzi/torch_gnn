25756a67687048340dbf6d9f26f2eb0beec3443b
Matteo Tiezzi <tiezzi7matteo@gmail.com>
2021-07-15 21:16:56 +0200

    Update pygnn.py

--- pygnn.py
+++ pygnn.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.nn.modules.utils import _pair
 from torch.nn import init
 import math
-from .net import MLP, StateTransition
+from net import MLP, StateTransition
 
 
 class GNN(nn.Module):
